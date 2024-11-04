28a11e7e07bd5aecf0dfc4ab56b20add9815e1ed
Rafal Owczarski <R.R.Owczarski@student.tudelft.nl>
2024-11-04 10:03:00 +0100

    Fix tests.

--- poc/src/test/java/allcount/poc/core/configuration/OpenBankingIntegrationTestConfiguration.java
+++ poc/src/test/java/allcount/poc/core/configuration/OpenBankingIntegrationTestConfiguration.java
@@ -1,6 +1,7 @@
 package allcount.poc.core.configuration;
 
 import allcount.poc.openbankingoauth.mapper.OpenBankingBankToBaseUriMapper;
+import allcount.poc.openbankingoauth.mapper.OpenBankingBankToSimulationMapper;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,5 +24,16 @@ public class OpenBankingIntegrationTestConfiguration {
     @Primary
     public OpenBankingBankToBaseUriMapper openBankingBankToBaseUriMapper() {
         return Mockito.mock(OpenBankingBankToBaseUriMapper.class);
+    }
+
+    /**
+     * Creates a mock of the OpenBankingBankToBaseUriMapper.
+     *
+     * @return the mock of the OpenBankingBankToBaseUriMapper
+     */
+    @Bean
+    @Primary
+    public OpenBankingBankToSimulationMapper openBankingBankToSimulationMapper() {
+        return Mockito.mock(OpenBankingBankToSimulationMapper.class);
     }
 }
